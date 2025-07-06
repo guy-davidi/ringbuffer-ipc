5221a3246aa45765f172fc528fa1a4a5c0ce01fd
Guy Davidi <guydav1111@gmail.com>
2025-07-06 23:07:40 +0300

    Use Atomic
    Signed-off-by: Guy Davidi <guydav1111@gmail.com>

--- include/ring_buffer.h
+++ include/ring_buffer.h
@@ -1,12 +1,9 @@
-/*
- * ring_buffer.h - Inter-CPU Ring Buffer
+/**
+ * @file ring_buffer.h
+ * @brief Lock-free ring buffer for inter-CPU/process communication.
  *
- * Copyright (C) 2025 Guy Davidi
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License.
+ * This ring buffer is designed for single-producer, single-consumer scenarios.
+ * Safe for inter-CPU shared memory when used with C11 atomics.
  */
 
 #ifndef RING_BUFFER_H
@@ -14,28 +11,97 @@
 
 #include <stdint.h>
 #include <stddef.h>
-
-#define RING_BUFFER_SIZE 1024
-#define RING_BUFFER_OK    0
-#define RING_BUFFER_FULL  1
-#define RING_BUFFER_EMPTY 2
-
-typedef struct {
-    volatile uint32_t head;
-    volatile uint32_t tail;
-    volatile uint8_t data[RING_BUFFER_SIZE];
-} ring_buffer_t;
+#include <stdatomic.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/** Return codes for ring buffer operations */
+enum {
+    RING_BUFFER_OK = 0,    /**< Operation successful */
+    RING_BUFFER_FULL = 1,  /**< Buffer is full */
+    RING_BUFFER_EMPTY = 2  /**< Buffer is empty */
+};
+
+/** Ring buffer size (must be a power of two for best performance) */
+#define RING_BUFFER_SIZE 1024
+
+/**
+ * @struct ring_buffer_t
+ * @brief Ring buffer object.
+ *
+ * @var ring_buffer_t::head
+ *      Index of the next element to read (consumer).
+ * @var ring_buffer_t::tail
+ *      Index of the next element to write (producer).
+ * @var ring_buffer_t::data
+ *      Buffer data storage.
+ */
+typedef struct ring_buffer {
+    atomic_uint head;
+    atomic_uint tail;
+    uint8_t data[RING_BUFFER_SIZE];
+} ring_buffer_t;
+
+/**
+ * @brief Initialize the ring buffer (sets head and tail to zero).
+ * @param buffer Pointer to ring buffer.
+ */
+void ring_buffer_init(ring_buffer_t *buffer);
+
+/**
+ * @brief Send a single byte to the ring buffer.
+ * @param buffer Pointer to ring buffer.
+ * @param val Byte to send.
+ * @return RING_BUFFER_OK if success, RING_BUFFER_FULL if buffer is full.
+ */
 int ring_buffer_send(ring_buffer_t *buffer, uint8_t val);
+
+/**
+ * @brief Receive a single byte from the ring buffer.
+ * @param buffer Pointer to ring buffer.
+ * @param out Pointer to output variable.
+ * @return RING_BUFFER_OK if success, RING_BUFFER_EMPTY if buffer is empty.
+ */
 int ring_buffer_recv(ring_buffer_t *buffer, uint8_t *out);
+
+/**
+ * @brief Send multiple bytes to the ring buffer.
+ * @param buffer Pointer to ring buffer.
+ * @param src Data to send.
+ * @param len Number of bytes to send.
+ * @return Number of bytes actually sent.
+ */
 int ring_buffer_send_bulk(ring_buffer_t *buffer, const uint8_t *src, int len);
+
+/**
+ * @brief Receive multiple bytes from the ring buffer.
+ * @param buffer Pointer to ring buffer.
+ * @param dst Destination buffer.
+ * @param len Number of bytes to receive.
+ * @return Number of bytes actually received.
+ */
 int ring_buffer_recv_bulk(ring_buffer_t *buffer, uint8_t *dst, int len);
+
+/**
+ * @brief Reset the ring buffer to empty state.
+ * @param buffer Pointer to ring buffer.
+ */
 void ring_buffer_reset(ring_buffer_t *buffer);
+
+/**
+ * @brief Get number of bytes currently stored in buffer.
+ * @param buffer Pointer to ring buffer.
+ * @return Number of bytes used.
+ */
 int ring_buffer_used(const ring_buffer_t *buffer);
+
+/**
+ * @brief Get number of bytes available for writing.
+ * @param buffer Pointer to ring buffer.
+ * @return Number of bytes free.
+ */
 int ring_buffer_free(const ring_buffer_t *buffer);
 
 #ifdef __cplusplus
--- src/ring_buffer.c
+++ src/ring_buffer.c
@@ -1,93 +1,97 @@
-/*
- * ring_buffer.c - Inter-CPU Ring Buffer
- *
- * Copyright (C) 2025 Guy Davidi
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License.
+/**
+ * @file ring_buffer.c
+ * @brief Lock-free ring buffer implementation (see ring_buffer.h).
  */
 
 #include "ring_buffer.h"
 
-static inline void memory_barrier() {
-    __asm__ volatile ("" ::: "memory");
+void ring_buffer_init(ring_buffer_t *buffer)
+{
+    atomic_store_explicit(&buffer->head, 0, memory_order_relaxed);
+    atomic_store_explicit(&buffer->tail, 0, memory_order_relaxed);
 }
 
-int ring_buffer_send(ring_buffer_t *buffer, uint8_t val) {
-    uint32_t tail = buffer->tail;
-    uint32_t head = buffer->head;
-    uint32_t next_tail = (tail + 1) % RING_BUFFER_SIZE;
+int ring_buffer_send(ring_buffer_t *buffer, uint8_t val)
+{
+    uint32_t tail = atomic_load_explicit(&buffer->tail, memory_order_relaxed);
+    uint32_t head = atomic_load_explicit(&buffer->head, memory_order_acquire);
 
+    uint32_t next_tail = (tail + 1) % RING_BUFFER_SIZE;
     if (next_tail == head)
         return RING_BUFFER_FULL;
 
     buffer->data[tail] = val;
-    memory_barrier();
-    buffer->tail = next_tail;
+    atomic_thread_fence(memory_order_release);
+    atomic_store_explicit(&buffer->tail, next_tail, memory_order_release);
 
     return RING_BUFFER_OK;
 }
 
-int ring_buffer_recv(ring_buffer_t *buffer, uint8_t *out) {
-    uint32_t head = buffer->head;
-    uint32_t tail = buffer->tail;
+int ring_buffer_recv(ring_buffer_t *buffer, uint8_t *out)
+{
+    uint32_t head = atomic_load_explicit(&buffer->head, memory_order_relaxed);
+    uint32_t tail = atomic_load_explicit(&buffer->tail, memory_order_acquire);
 
     if (head == tail)
         return RING_BUFFER_EMPTY;
 
     *out = buffer->data[head];
-    memory_barrier();
-    buffer->head = (head + 1) % RING_BUFFER_SIZE;
+    atomic_thread_fence(memory_order_release);
+    atomic_store_explicit(&buffer->head, (head + 1) % RING_BUFFER_SIZE, memory_order_release);
 
     return RING_BUFFER_OK;
 }
 
-int ring_buffer_send_bulk(ring_buffer_t *buffer, const uint8_t *src, int len) {
+int ring_buffer_send_bulk(ring_buffer_t *buffer, const uint8_t *src, int len)
+{
     int sent = 0;
     while (sent < len) {
-        uint32_t tail = buffer->tail;
-        uint32_t head = buffer->head;
+        uint32_t tail = atomic_load_explicit(&buffer->tail, memory_order_relaxed);
+        uint32_t head = atomic_load_explicit(&buffer->head, memory_order_acquire);
         uint32_t next_tail = (tail + 1) % RING_BUFFER_SIZE;
 
         if (next_tail == head)
             break; // full
 
         buffer->data[tail] = src[sent++];
-        memory_barrier();
-        buffer->tail = next_tail;
+        atomic_thread_fence(memory_order_release);
+        atomic_store_explicit(&buffer->tail, next_tail, memory_order_release);
     }
     return sent;
 }
 
-int ring_buffer_recv_bulk(ring_buffer_t *buffer, uint8_t *dst, int len) {
+int ring_buffer_recv_bulk(ring_buffer_t *buffer, uint8_t *dst, int len)
+{
     int recvd = 0;
     while (recvd < len) {
-        uint32_t head = buffer->head;
-        uint32_t tail = buffer->tail;
+        uint32_t head = atomic_load_explicit(&buffer->head, memory_order_relaxed);
+        uint32_t tail = atomic_load_explicit(&buffer->tail, memory_order_acquire);
+
         if (head == tail)
             break; // empty
 
         dst[recvd++] = buffer->data[head];
-        memory_barrier();
-        buffer->head = (head + 1) % RING_BUFFER_SIZE;
+        atomic_thread_fence(memory_order_release);
+        atomic_store_explicit(&buffer->head, (head + 1) % RING_BUFFER_SIZE, memory_order_release);
     }
     return recvd;
 }
 
-void ring_buffer_reset(ring_buffer_t *buffer) {
-    buffer->head = 0;
-    buffer->tail = 0;
-    memory_barrier();
+void ring_buffer_reset(ring_buffer_t *buffer)
+{
+    atomic_store_explicit(&buffer->head, 0, memory_order_relaxed);
+    atomic_store_explicit(&buffer->tail, 0, memory_order_relaxed);
+    atomic_thread_fence(memory_order_release);
 }
 
-int ring_buffer_used(const ring_buffer_t *buffer) {
-    uint32_t head = buffer->head;
-    uint32_t tail = buffer->tail;
+int ring_buffer_used(const ring_buffer_t *buffer)
+{
+    uint32_t head = atomic_load_explicit(&buffer->head, memory_order_acquire);
+    uint32_t tail = atomic_load_explicit(&buffer->tail, memory_order_acquire);
     return (tail + RING_BUFFER_SIZE - head) % RING_BUFFER_SIZE;
 }
 
-int ring_buffer_free(const ring_buffer_t *buffer) {
+int ring_buffer_free(const ring_buffer_t *buffer)
+{
     return RING_BUFFER_SIZE - 1 - ring_buffer_used(buffer);
 }
